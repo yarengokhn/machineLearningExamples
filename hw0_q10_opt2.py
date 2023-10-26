cc4462e0a2a642897619d5d9624420919d76de17
Arda Atakan Uçan <ardaatakanucan@gmail.com>
2023-10-26 19:38:45 +0300

    soru 10.b Arda'nın çözümü

---
+++ hw0_q10_opt2.py
@@ -0,0 +1,21 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+n = 40000
+Z = np.random.randn(n)
+
+ax = plt.gca()
+ax.set_xlim((-3, 3))
+plt.xlabel("Observations")
+plt.ylabel("Probability")
+
+plt.step(sorted(Z), np.arange(1, n + 1) / float(n), label="Gaussian")
+
+# ============================ Question 10.b ============================
+for k in [1, 8, 64, 512]:
+    Zk = np.sum(np.sign(np.random.randn(n, k)) * np.sqrt(1.0 / k), axis=1)
+    plt.step(sorted(Zk), np.arange(1, n + 1) / float(n), label=f"k = {k}")
+# =======================================================================
+
+plt.legend()
+plt.show()
